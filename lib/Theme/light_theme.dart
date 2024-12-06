9b447222b7aa8f1b8c5d223176f3af31e409cd0b
Kavindualwis <131531468+kavindualwis@users.noreply.github.com>
2024-12-07 03:44:11 +0530

    aa

--- lib/Theme/light_theme.dart
+++ lib/Theme/light_theme.dart
@@ -2,10 +2,10 @@ import 'package:flutter/material.dart';
 
 ThemeData lightMode = ThemeData(
   colorScheme: const ColorScheme.light(
-    surface: Color.fromARGB(243, 244, 250, 251),
+    surface: Color.fromARGB(200, 244, 250, 251),
     primary: Colors.white,
     secondary: Colors.teal,
-    onPrimary: Colors.grey,
+    onPrimary: Color.fromARGB(255, 124, 124, 124),
     onPrimaryFixed: Colors.black,
   ),
 );
